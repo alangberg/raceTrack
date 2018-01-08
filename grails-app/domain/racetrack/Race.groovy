4b7e6def8802cd314d75ee61cc85bc307ad6aa52
Andres Langberg <andreslangberg@gmail.com>
2018-01-07 22:36:52 -0300

    Add Race and Registration Controllers

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -4,10 +4,23 @@ class Race {
     String name
     Date startDate
     String state
+    String city
     BigDecimal distance
     BigDecimal cost
     Integer maxRunners = 10000
 
     static constraints = {
+        name()
+        startDate()
+        city()
+        state()
+        distance()
+        cost()
+        maxRunners()
     }
+
+
+
+
+
 }
