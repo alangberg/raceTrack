a27393dbf8dc37ec4733909fc0d85f9f112337a2
Andres Langberg <andreslangberg@gmail.com>
2018-01-07 23:20:34 -0300

    Add costraints to RaceController

--- grails-app/domain/racetrack/Race.groovy
+++ grails-app/domain/racetrack/Race.groovy
@@ -10,17 +10,13 @@ class Race {
     Integer maxRunners = 10000
 
     static constraints = {
-        name()
-        startDate()
+        name(blank:false, maxSize:50)
+        startDate(validator: {return (it > new Date())})
         city()
-        state()
-        distance()
-        cost()
-        maxRunners()
+        state(inList: ["GA", "NC", "SC", "VA"])
+        distance(min:0.0)
+        cost(min:0.0, max:100.0)
+        maxRunners(min:0, max:100000)
     }
-
-
-
-
 
 }
